d47bb66b69aa141bc425fdecf23195399ccd18f5
UnknownMaillet <62292183+arnaudmaillet@users.noreply.github.com>
2025-02-21 18:04:56 +0100

    Initial Commit

---
+++ prototype/prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototypeApp.swift
+//  prototype
+//
+//  Created by Arnaud Maillet on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
